b17e8c1695eb7e68382804667ae0dbfed1e5d0ea
Sugandh Kumar <64632983+sugandhkumar01@users.noreply.github.com>
2022-08-31 14:39:57 +0530

    v3

--- demo.js
+++ demo.js
@@ -1,9 +1,10 @@
-var expect  = require('chai').expect;
-var request = require('request');
 
-it('Main page content', function(done) {
-    request('http://localhost:8080' , function(error, response, body) {
-        expect(body).to.equal('Hello World');
-        done();
-    });
-});
+var test = require(unit.js);
+var str = 'hello, world!';
+
+test.string(str).startWith('hello');
+
+if (test.string(str).startWith('hello')){
+    console.log("Passed");
+}
+
